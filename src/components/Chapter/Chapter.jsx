1ebdbf9108bfbeecd34cc0806406a54dbd2375d1
KariminK <103779342+KariminK@users.noreply.github.com>
2024-06-04 21:49:41 +0200

    chapter component done

--- src/components/Chapter/Chapter.jsx
+++ src/components/Chapter/Chapter.jsx
@@ -1,9 +1,26 @@
 import PropTypes from "prop-types";
-const Chapter = ({ id }) => {
-  return <main id={id}></main>;
+import CodeSection from "../CodeSection/CodeSection";
+const Chapter = ({ id, sections }) => {
+  let sectionEls = [];
+  if (Array.isArray(sections)) {
+    sectionEls = sections.map((section) => {
+      return (
+        <CodeSection
+          key={crypto.randomUUID()}
+          title={section.title}
+          description={section.description}
+          code={section.code}
+          color={section.color}
+          table={section.table}
+        />
+      );
+    });
+  }
+  return <main id={id}>{sectionEls}</main>;
 };
 Chapter.propTypes = {
   id: PropTypes.string,
+  sections: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default Chapter;
--- src/components/Chapter/Chapter.test.jsx
+++ src/components/Chapter/Chapter.test.jsx
@@ -7,5 +7,34 @@ describe("Chapter", () => {
     render(<Chapter id="exampleId" />);
     expect(screen.getByRole("main").id).toBe("exampleId");
   });
-  it("Should render bunch of CodeSections based on data given in props", () => {});
+  it("Should render bunch of CodeSections based on data given in props", () => {
+    render(
+      <Chapter
+        sections={[
+          {
+            title: "Example1title",
+            description: "Example1description",
+            code: `example1code`,
+            color: "cyan",
+          },
+          {
+            title: "Example2title",
+            description: "Example2description",
+            code: `example2code`,
+            color: "emerald",
+          },
+        ]}
+      />
+    );
+    const headings = screen.getAllByRole("heading");
+    const descriptions = screen.getAllByRole("paragraph");
+    const codes = screen.getAllByRole("code");
+
+    expect(headings[0].textContent).toBe("Example1title");
+    expect(headings[1].textContent).toBe("Example2title");
+    expect(descriptions[0].textContent).toBe("Example1description");
+    expect(descriptions[1].textContent).toBe("Example2description");
+    expect(codes[0].textContent).toBe("example1code");
+    expect(codes[1].textContent).toBe("example2code");
+  });
 });
