43878a7106e99c5ea6a51b4226cdb85835d5465d
KariminK <103779342+KariminK@users.noreply.github.com>
2024-06-10 22:50:29 +0200

    correlated contents to data file

--- src/pages/Obiektowe/Obiektowe.jsx
+++ src/pages/Obiektowe/Obiektowe.jsx
@@ -1,17 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Contents from "../../components/Contents/Contents";
+import obiektoweData from "/src/data/obiektowe.json";
 
 const Obiektowe = () => {
+  const chapters = Object.keys(obiektoweData).map((chapter) => {
+    return { to: chapter, text: chapter.toUpperCase() };
+  });
   return (
     <>
       <h1 className="text-5xl font-kreon text-green-400 text-center mt-40 mb-10">
         Programowanie obiektowe
       </h1>
       <Contents
-        chapters={[
-          { to: "./pliki c++", text: "Obsługa plików" },
-          { to: "./klasy php", text: "Klasy i obiekty w PHP" },
-        ]}
+        chapters={chapters}
         colors={{
           mainList: "text-green-100 border-green-400 shadow-green-500",
           heading: "text-green-400",
@@ -20,6 +21,12 @@ const Obiektowe = () => {
         optional={"mb-20"}
       />
       <Outlet />
+      <Link
+        className="text-green-500 block text-center my-6 hover:text-green-300 transition-colors text-xl"
+        to={"/"}
+      >
+        Powrót na stronę główną
+      </Link>
     </>
   );
 };
