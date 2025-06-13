f582301868383ef4a8a3b65bdbd750c4431d3208
KariminK <103779342+KariminK@users.noreply.github.com>
2025-06-13 22:26:59 +0200

    new material, footer fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Footer = ({ theme }) => {
   return (
-    <footer className="absolute bottom-0 left-0 right-0 my-5 text-sm text-center text-neutral-100">
+    <footer className="my-5 text-sm text-center text-neutral-100">
       <p>
         Stworzone przez{" "}
         <a
